30f992ba4a7d5f747546680bee6f50612a81726d
Eduard <edyabrug91@gmail.com>
2024-05-11 23:34:12 +0300

    add inner to popup

--- wp-content/themes/lettersgallery/templates/popup.php
+++ wp-content/themes/lettersgallery/templates/popup.php
@@ -1,21 +1,22 @@
 <div class="backdrop is-hidden" id="popup-backdrop">
     <div class="popup">
+      <div class="inner position-relative">
+          <?=
+          get_template_part('templates/popupForm');
+          get_template_part('templates/popupSuccess');
+          ?>
 
-        <?=
-        get_template_part('templates/popupForm');
-        get_template_part('templates/popupSuccess');
-        ?>
+          <div class="popup-button-wrapper">
+              <div class="hide-popup">
+                  <svg class="hide-popup__icon" width="22" height="22">
+                      <use href="<?php get_image('sprite.svg#close'); ?>"></use>
+                  </svg>
+              </div>
+          </div>
 
-        <div class="popup-button-wrapper">
-            <div class="hide-popup">
-                <svg class="hide-popup__icon" width="22" height="22">
-                    <use href="<?php get_image('sprite.svg#close'); ?>"></use>
-                </svg>
-            </div>
-        </div>
-
-        <div class="loader-wrapper position-absolute start-50 top-50 translate-middle">
-            <div class="loader"></div>
-        </div>
+          <div class="loader-wrapper position-absolute start-50 top-50 translate-middle">
+              <div class="loader"></div>
+          </div>
+      </div>
     </div>
 </div>
