c24789091252ad6e0bb2c28b374fbbf92badad55
Eduard <edyabrug91@gmail.com>
2024-07-13 18:36:23 +0300

    changed common hero section

--- wp-content/themes/lettersgallery/sections/heroSection.php
+++ wp-content/themes/lettersgallery/sections/heroSection.php
@@ -7,20 +7,11 @@ $gallery = get_field("hero_gallery");
         <?php
         get_template_part('templates/breadcrumbs');
 
-        if ($gallery || is_singular("product")) {
+        if ($gallery) {
             ?>
             <div class="swiper hero-swiper">
                 <ul class="swiper-wrapper">
                     <?php
-                    if (!$gallery) {
-                        $image_url = wp_get_attachment_image_src(5, 'full');
-                        ?>
-                        <li class="swiper-slide hero-slide zoom-js" style="background-image: url(<?= $image_url[0]; ?>)">
-                            <?= wp_get_attachment_image(5, 'full', false, array('class' => '')); ?>
-                        </li>
-                        <?php
-                    }
-
                     foreach ($gallery as $image) {
                         ?>
                         <li class="swiper-slide hero-slide zoom-js"
